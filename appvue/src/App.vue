6af31393442b7940334326cc44e0da431f70591c
Anderson Luigui <alns1536@hotmail.com>
2023-06-14 16:15:59 -0300

    Aprendendo Vue continuação

--- appvue/src/App.vue
+++ appvue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Este é o Vue.js App" visibilidade="false"/>
   </div>
 </template>
 
