df0a4c55a6165f3dd8b5d209ece2f321a2737c9f
ANDERSON LUIGUI <alns1536@hotmail.com>
2023-06-20 20:59:51 -0300

    First commit

--- jogo-da-forca-vue-js/src/App.vue
+++ jogo-da-forca-vue-js/src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Jogo da Forca Dev Anderson Luigui </h1>
+
+    <section v-if="tela === 'inicio'" id="inicio">
+      <Formulario/>
+    </section>
+
+    <section v-if="tela === 'jogo'"  id="jogo" >
+      Jogo
+    </section>
+  
   </div>
 </template>
 
+
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import './css/global.css';
+import Formulario from './components/Formulario.vue';
 
 export default {
   name: 'App',
+  data(){
+    return{
+      tela: 'inicio'
+    }
+  },
   components: {
-    HelloWorld
+    
   }
 }
 </script>
 
+
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 </style>
---
+++ jogo-da-forca-vue-js/src/components/Formulario.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Formulario'
+
+}
+</script>
+
+<style>
+
+</style>
